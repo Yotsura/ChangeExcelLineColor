75bf0c322ee2dbe17e29afe4fe1b7ea65f50256f
yuminaka <kentaro.yuminaka3485@cks.co.jp>
2021-09-15 10:44:59 +0900

    ツールひとまず完成

---
+++ ChangeExcelLineColor/ControlExcel.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using ClosedXML.Excel;
+
+namespace ChangeExcelLineColor
+{
+    public static class ControlExcel
+    {
+        public static void ChangeAllBooks(string filename)
+        {
+            var fileInfo = new FileInfo(filename);
+            var newFileName = $"{fileInfo.Directory}\\FIXED_{fileInfo.Name}";
+            File.Copy(filename, newFileName, true);
+            using (var book = new XLWorkbook(newFileName))
+            {
+                var theme = book.Theme;
+                foreach (var sheet in book.Worksheets)
+                {
+                    var printArea = sheet.PageSetup.PrintAreas.FirstOrDefault();
+                    if (printArea == null) continue;
+                    ChangeRanges(printArea, theme);
+                    //foreach (var printArea in sheet.PageSetup.PrintAreas)
+                    //{
+                    //    if (printArea == null) continue;
+                    //    ChangeRanges(printArea, theme);
+                    //}
+                }
+                book.SaveAs(newFileName);
+            }
+            Console.WriteLine($"修正済みファイル：{new FileInfo(newFileName).Name}");
+        }
+
+        private static void ChangeRanges(IXLRange range, IXLTheme theme)
+        {
+            foreach(var targetCell in range.Cells())
+            {
+                ChangeCell(targetCell, theme);
+            }
+        }
+
+        private static void ChangeCell(IXLCell targetCell, IXLTheme theme)
+        {
+            if (targetCell.Style.Border.BottomBorder == XLBorderStyleValues.None)
+            {
+                targetCell.Style.Border.BottomBorder = XLBorderStyleValues.Thin;
+                targetCell.Style.Border.BottomBorderColor = XLColor.White;
+            }
+            else if (targetCell.Style.Border.BottomBorderColor.IsNonColored(theme))
+                targetCell.Style.Border.BottomBorderColor = XLColor.Black;
+            if (targetCell.Style.Border.TopBorder == XLBorderStyleValues.None)
+            {
+                targetCell.Style.Border.TopBorder = XLBorderStyleValues.Thin;
+                targetCell.Style.Border.TopBorderColor = XLColor.White;
+            }
+            else if (targetCell.Style.Border.TopBorderColor.IsNonColored(theme))
+                targetCell.Style.Border.TopBorderColor = XLColor.Black;
+            if (targetCell.Style.Border.LeftBorder == XLBorderStyleValues.None)
+            {
+                targetCell.Style.Border.LeftBorder = XLBorderStyleValues.Thin;
+                targetCell.Style.Border.LeftBorderColor = XLColor.White;
+            }
+            else if (targetCell.Style.Border.LeftBorderColor.IsNonColored(theme))
+                targetCell.Style.Border.LeftBorderColor = XLColor.Black;
+            if (targetCell.Style.Border.RightBorder == XLBorderStyleValues.None)
+            {
+                targetCell.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+                targetCell.Style.Border.RightBorderColor = XLColor.White;
+            }
+            else if (targetCell.Style.Border.RightBorderColor.IsNonColored(theme))
+                targetCell.Style.Border.RightBorderColor = XLColor.Black;
+
+            if (targetCell.Style.Border.DiagonalBorder != XLBorderStyleValues.None &&
+                targetCell.Style.Border.DiagonalBorderColor.IsNonColored(theme))
+                targetCell.Style.Border.DiagonalBorderColor = XLColor.Black;
+        }
+
+        private static bool IsNonColored(this XLColor xlColor, IXLTheme theme)
+        {
+            switch (xlColor.ColorType)
+            {
+                case XLColorType.Color:
+                    return xlColor.ToString() == "00000000";
+                case XLColorType.Theme:
+                    var xlThemeColor = theme.ResolveThemeColor(xlColor.ThemeColor).ToString();
+                    var black = XLColor.Black.ToString();
+                    return xlThemeColor == black;
+                case XLColorType.Indexed:
+                    var color = XLColor.IndexedColors[xlColor.Indexed];
+                    if (xlColor.Indexed >= 64) return true;
+                    break;
+            }
+            return false;
+        }
+    }
+}
---
+++ ChangeExcelLineColor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeExcelLineColor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ControlExcel.ChangeAllBooks(@"C:\Users\pc-yuminaka\Desktop\TestBook.xlsx");
+            var files = System.Environment.GetCommandLineArgs();
+            var filenames = new List<string>();
+            if (files.Length > 1)
+            {
+                filenames = files.Skip(1).ToList();
+            }
+            else
+            {
+                Console.WriteLine(
+                    "「ChangeExcelLineColor.exe」に直接EXCELファイルをドラックアンドドロップしてください。※複数可能\r\n" +
+                    "＜仕様＞\r\n印刷範囲内のすべてのセルについて以下の変更を行います。\r\n" +
+                    "線のない辺　→　白の線を設定　※斜め線は対象外\r\n" +
+                    "色自動の線あり　→　線の色を黒に変更\r\n" +
+                    "色が自動・黒以外の線　→　そのまま\r\n\r\n※この画面はなにかキーを押せば閉じます。");
+                Console.ReadKey();
+                return;
+            }
+            var xlsxfiles = filenames.Where(file => file.EndsWith("xlsx") || file.EndsWith("xlsm") || file.EndsWith("xls")).ToList();
+            if (xlsxfiles.Count() < 1)
+            {
+                Console.WriteLine("EXCELファイルがありません。");
+                Console.ReadKey();
+                return;
+            }
+
+            xlsxfiles.ForEach(file => ControlExcel.ChangeAllBooks(file));
+            Console.WriteLine("作業完了");
+            Console.ReadKey();
+        }
+    }
+}
